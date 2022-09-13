33afd8316d78c82f2f34a144f0a0bd5e01bba57d
madisonlowe <mcourteney@live.com>
2022-09-13 19:00:00 +0100

    Started coding, notes in App.tsx.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 import EditableHeader from "../EditableHeader/EditableHeader";
 import EditableBlock from "../EditableBlock/EditableBlock";
 import "./App.css";
@@ -9,7 +9,17 @@ function App() {
     setValue: any;
   }
 
+  interface pageComponents {
+    components: any;
+    setComponents: (value: any) => void;
+  }
+
   const [value, setValue] = useState<editableValue>();
+  const [components, setComponents] = useState<pageComponents>(["Tester"]);
+
+  function addComponent() {
+    setComponents([...components, "Test Component"]);
+  }
 
   return (
     <main className="App">
@@ -20,3 +30,12 @@ function App() {
 }
 
 export default App;
+
+/*
+AIMS:
+- we create a state that tracks how many components we have
+- on a certain input, we add another component to the array of components tracked in state
+- we render those components on screen
+- we can later take those components away from the array
+- this is meant to mimic how, on enter in notion, a new component comes onto screen
+*/
